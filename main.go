f0f08df155282a6853648d304b42e07744e2bc76
anraku <anraku90@gmail.com>
2017-09-27 17:57:57 +0900

    Go言語によるWebアプリケーション開発 P132まで

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	mgo "gopkg.in/mgo.v2"
+)
 
 func main() {
 	fmt.Println("vim-go")
+}
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイアル中：localhost")
+	// MongoDBに接続
+	db, err = mgo.Dial("localhost") // HACK: URLを指定できる形にしたい
+	return err
+}
+func closedb() {
+	// MongoDBを閉じる
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+}
+
+// 投票情報を取得するための構造体
+type poll struct {
+	Options []string // 選択肢
+}
+
+func loadOptions() ([]string, error) {
+	var options []string                                 // 投票情報を格納するためのスライス
+	iter := db.DB("ballots").C("polls").Find(nil).Iter() // 検索結果のiteratorを取得
+	// 検索結果を保持するオブジェクト
+	// AllではなくFindの結果を保持することで、1つのオブジェクトで結果を保持できる
+	var p poll
+	for iter.Next(&p) {
+		// 検索結果を一つずつoptionsに追加する
+		options = append(options, p.Optilns...)
+	}
+	iter.Close()
+	return option, iter.Err()
 }
--- twitter.go
+++ twitter.go
@@ -1,6 +1,18 @@
 package main
 
-import "net"
+import (
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/garyburd/go-oauth/oauth"
+	"github.com/joeshaw/envdecode"
+)
 
 var conn net.Conn
 
@@ -20,6 +32,7 @@ func dial(netw, addr string) (net.Conn, error) {
 	return netc, nil
 }
 
+// 検索結果のレスポンスを保持するための変数
 var reader io.ReadCloser
 
 // Twitterとの接続をクリーンアップするための関数
@@ -32,21 +45,25 @@ func closeConn() {
 	}
 }
 
+// TwitterAPIを使ったリクエストを送るためのオブジェクト
 var (
 	authClient *oauth.Client
 	creds      *oauth.Credentials
 )
 
 func setupTwitterAuth() {
+	// TwitterAPIを使うための認証情報
 	var ts struct {
 		ConsumerKey    string `env:"SP_TWITTER_KEY"`
 		ConsumerSecret string `env:"SP_TWITTER_SECRET"`
 		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN"`
 		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET"`
 	}
+	// 環境変数が有効かどうかチェック
 	if err := envdecode.Decode(&ts); err != nil {
 		log.Fatalln(err)
 	}
+	// 各種認証情報をオブジェクトに設定
 	creds = &oauth.Credentials{
 		Token:  ts.AccessToken,
 		Secret: ts.AccessSecret,
@@ -56,5 +73,74 @@ func setupTwitterAuth() {
 			Token:  ts.ConsumerKey,
 			Secret: ts.ConsumerSecret,
 		},
+	}
+}
+
+// Twitterにリクエストを送るためのリクエスト
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
+
+// Twitterにリクエストを送信
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	// 初期化処理をsync.Onceでラップすることで1回のみ実行とする
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+	return httpClient.Do(req)
+}
+
+type tweet struct {
+	Text string
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("選択肢の読み込みに失敗しました：", err)
+		return
+	}
+	u, err := url.Parse("https://stream.twitter.com/1.1/statuses/filter.json")
+	if err != nil {
+		log.Println("URLの解析に失敗しました：", err)
+		return
+	}
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+	if err != nil {
+		log.Println("検索のリクエストの作成に失敗しました：", err)
+		return
+	}
+	resp, err := makeRequest(req, query)
+
+	if err != nil {
+		log.Println("検索のリクエストに失敗しました", err)
+		return
+	}
+
+	reader = resp.Body
+	decoder := json.NewDecoder(reader)
+	for {
+		var tweet tweet
+		if err := decoder.Decode(&tweet); err != nil {
+			break
+		}
+		for _, option := range options {
+			if strings.Contains(strings.ToLower(tweet.Text), strings.ToLower(option)) {
+				log.Println("投票:", option)
+				votes <- option
+			}
+		}
 	}
 }
